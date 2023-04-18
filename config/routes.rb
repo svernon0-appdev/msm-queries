a1b32e74aca1caf48ecee9c2476d009bf3c08fcf
Spencer Vernon <svernon0@chicagobooth.edu>
2023-04-18 16:31:04 +0000

    Edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
 end
